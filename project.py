b28b760756d5039c5659821cab344bdc6dda4ae3
Eshika Kotha <eshika.ebnat@gmail.com>
2025-08-31 20:59:01 +0600

    mainfunctions

---
+++ project.py
@@ -0,0 +1,68 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+import math, random, time
+
+## Window dimensions and aspect ratio
+WINDOW_W, WINDOW_H = 1280, 650
+ASPECT = WINDOW_W / WINDOW_H
+GRID_LENGTH = 1400
+fovY_default = 120.0
+fovY_zoom = 30.0
+fovY_zoom_3p = 90.0  # mild zoom in 3P
+
+first_person = False
+scope_active = False
+fire_pressed = False  # fire cooldown
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(WINDOW_W, WINDOW_H)
+    glutInitWindowPosition(50, 50)
+    glutCreateWindow(b"Shoot & Survive - 3D Game")
+    glEnable(GL_DEPTH_TEST)
+    glutDisplayFunc(showScreen)
+    glutKeyboardFunc(on_key_down)
+    glutKeyboardUpFunc(on_key_up)
+    glutSpecialFunc(specialKeyListener)
+    glutMouseFunc(mouseListener)
+    glutIdleFunc(idle)
+    reset_game()
+    glutMainLoop()
+
+if __name__ == "__main__":
+    main()
